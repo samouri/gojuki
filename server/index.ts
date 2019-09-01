55a168a46fcef07258663fc146a9add62ef0fbb7
Jake F - @samouri <samouri@users.noreply.github.com>
2019-09-01 15:33:25 -0400

    time 2 make game

--- server/index.ts
+++ server/index.ts
@@ -40,7 +40,6 @@ function sendGameUpdates() {
   for (const [peerId, tickMessage] of Object.entries(state.getTickData(Array.from(piers.keys())))) {
     // todo: is it okay that getTickData also increases the serverTick?
     const peer = piers.get(peerId)
-    // jitter(100).then(() => peer.send(JSON.stringify(tickMessage)))
     peer.send(JSON.stringify(tickMessage))
   }
 }
