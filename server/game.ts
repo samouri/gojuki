e93d3e5e38bb4613cc56e02bcf372c05073741c5
Jake Fried <samouri@users.noreply.github.com>
2019-12-22 18:22:35 -0500

    adopt prettier

--- server/game.ts
+++ server/game.ts
@@ -2,101 +2,112 @@
  * All of the game state that is shared between servers and all clients.
  * This means only the model (state).
  */
-import * as _ from "lodash"
+import * as _ from 'lodash'
 
 const GAME_DIMENSIONS = Object.freeze({ width: 769, height: 480 })
 export function getGameDimensions() {
-  return GAME_DIMENSIONS
+    return GAME_DIMENSIONS
 }
 
 export type World = {
-  players: { [id: string]: GamePlayer }
-  serverTick: number
+    players: { [id: string]: GamePlayer }
+    serverTick: number
 }
 
 export type PlayerInput = { left: boolean; right: boolean; up: boolean }
 export type GamePlayer = {
-  playerNumber: 1 | 2 | 3 | 4
-  playerName: string
-  x: number
-  y: number
-  v: number
-  friction: number
-  turnSpeed: number
-  acceleration: number
-  rotation: number
+    playerNumber: 1 | 2 | 3 | 4
+    playerName: string
+    x: number
+    y: number
+    v: number
+    friction: number
+    turnSpeed: number
+    acceleration: number
+    rotation: number
 }
 
 const baseSize = 70
 export const PLAYER_CONFIG: { [id: number]: any } = {
-  1: {
-    color: "#E93F3F",
-    startPosition: { x: baseSize / 2, y: baseSize / 2, rotation: Math.PI },
-    basePosition: { x: 0, y: 0 },
-  },
-  2: {
-    color: "#38D183",
-    startPosition: {
-      x: getGameDimensions().width - baseSize / 2.0,
-      y: baseSize / 2,
-      rotation: Math.PI,
+    1: {
+        color: '#E93F3F',
+        startPosition: { x: baseSize / 2, y: baseSize / 2, rotation: Math.PI },
+        basePosition: { x: 0, y: 0 },
     },
-    basePosition: { x: getGameDimensions().width - baseSize, y: 0 },
-  },
-  3: {
-    color: "#3FD3E9",
-    startPosition: { x: baseSize / 2.0, y: getGameDimensions().height - baseSize / 2, rotation: 0 },
-    basePosition: { x: 0, y: getGameDimensions().height - baseSize },
-  },
-  4: {
-    color: "#E93FDB",
-    startPosition: {
-      x: getGameDimensions().width - baseSize / 2.0,
-      y: getGameDimensions().height - baseSize / 2,
-      rotation: 0,
+    2: {
+        color: '#38D183',
+        startPosition: {
+            x: getGameDimensions().width - baseSize / 2.0,
+            y: baseSize / 2,
+            rotation: Math.PI,
+        },
+        basePosition: { x: getGameDimensions().width - baseSize, y: 0 },
     },
-    basePosition: {
-      x: getGameDimensions().width - baseSize,
-      y: getGameDimensions().height - baseSize,
+    3: {
+        color: '#3FD3E9',
+        startPosition: {
+            x: baseSize / 2.0,
+            y: getGameDimensions().height - baseSize / 2,
+            rotation: 0,
+        },
+        basePosition: { x: 0, y: getGameDimensions().height - baseSize },
     },
-  },
+    4: {
+        color: '#E93FDB',
+        startPosition: {
+            x: getGameDimensions().width - baseSize / 2.0,
+            y: getGameDimensions().height - baseSize / 2,
+            rotation: 0,
+        },
+        basePosition: {
+            x: getGameDimensions().width - baseSize,
+            y: getGameDimensions().height - baseSize,
+        },
+    },
 }
 
-export function getDefaultPlayer(playerNum: 1 | 2 | 3 | 4, playerName: string): GamePlayer {
-  return {
-    playerNumber: playerNum,
-    playerName,
-    x: PLAYER_CONFIG[playerNum].startPosition.x,
-    y: PLAYER_CONFIG[playerNum].startPosition.y,
-    v: 0,
-    rotation: PLAYER_CONFIG[playerNum].startPosition.rotation,
-    friction: 0.9,
-    turnSpeed: 0.1,
-    acceleration: 0.2,
-  }
+export function getDefaultPlayer(
+    playerNum: 1 | 2 | 3 | 4,
+    playerName: string,
+): GamePlayer {
+    return {
+        playerNumber: playerNum,
+        playerName,
+        x: PLAYER_CONFIG[playerNum].startPosition.x,
+        y: PLAYER_CONFIG[playerNum].startPosition.y,
+        v: 0,
+        rotation: PLAYER_CONFIG[playerNum].startPosition.rotation,
+        friction: 0.9,
+        turnSpeed: 0.1,
+        acceleration: 0.2,
+    }
 }
 
-export function stepPlayer(world: World, playerId: string, inputs: Array<PlayerInput>) {
-  const gameDim = getGameDimensions()
-  inputs = [...inputs]
+export function stepPlayer(
+    world: World,
+    playerId: string,
+    inputs: Array<PlayerInput>,
+) {
+    const gameDim = getGameDimensions()
+    inputs = [...inputs]
 
-  const p = world.players[playerId]
-  while (!_.isEmpty(inputs)) {
-    const input = inputs.shift()
-    if (input.up) {
-      p.v += p.acceleration
+    const p = world.players[playerId]
+    while (!_.isEmpty(inputs)) {
+        const input = inputs.shift()
+        if (input.up) {
+            p.v += p.acceleration
+        }
+        p.v *= p.friction
+        p.x = p.x + Math.sin(p.rotation) * p.v
+        p.y = p.y + Math.cos(p.rotation) * -1 * p.v
+
+        if (input.left) {
+            p.rotation -= p.turnSpeed
+        } else if (input.right) {
+            p.rotation += p.turnSpeed
+        }
+
+        p.x = Math.min(Math.max(10, p.x), gameDim.width - 10)
+        p.y = Math.min(Math.max(10, p.y), gameDim.height - 10)
     }
-    p.v *= p.friction
-    p.x = p.x + Math.sin(p.rotation) * p.v
-    p.y = p.y + Math.cos(p.rotation) * -1 * p.v
-
-    if (input.left) {
-      p.rotation -= p.turnSpeed
-    } else if (input.right) {
-      p.rotation += p.turnSpeed
-    }
-
-    p.x = Math.min(Math.max(10, p.x), gameDim.width - 10)
-    p.y = Math.min(Math.max(10, p.y), gameDim.height - 10)
-  }
 }
--- server/state.ts
+++ server/state.ts
@@ -1,159 +1,186 @@
-import * as _ from "lodash"
-import { PlayerInput, World, getDefaultPlayer, stepPlayer } from "./game"
-import { jitter } from "./peers"
+import * as _ from 'lodash'
+import { PlayerInput, World, getDefaultPlayer, stepPlayer } from './game'
+import { jitter } from './peers'
 const maxPartySize = 2
 let serverTick = 0
 
 /* Messages & message creators */
-export type LOG_MESSAGE = { type: "LOG"; message: string }
-export type JOIN_PARTY_MESSAGE = { type: "JOIN_PARTY"; playerName: string; peerId: string }
-export type PLAYER_INPUTS = { type: "CLIENT_INPUTS"; input: PlayerInput }
+export type LOG_MESSAGE = { type: 'LOG'; message: string }
+export type JOIN_PARTY_MESSAGE = {
+    type: 'JOIN_PARTY'
+    playerName: string
+    peerId: string
+}
+export type PLAYER_INPUTS = { type: 'CLIENT_INPUTS'; input: PlayerInput }
 export type SERVER_TICK_MESSAGE = {
-  type: "SERVER_TICK"
-  world: null | World
-  party: null | Party
-  serverTick: number
-  clientTick: number
+    type: 'SERVER_TICK'
+    world: null | World
+    party: null | Party
+    serverTick: number
+    clientTick: number
 }
 
 export type HEARTBEAT_MESSAGE = {
-  type: "CLIENT_HEARTBEAT"
-  serverTick: number
-  clientTick: number
+    type: 'CLIENT_HEARTBEAT'
+    serverTick: number
+    clientTick: number
 }
 
 export type CLIENT_TICK_MESSAGE = {
-  type: "CLIENT_TICK"
-  inputs: Array<[number, PlayerInput]>
-  serverTick: number
-  clientTick: number
+    type: 'CLIENT_TICK'
+    inputs: Array<[number, PlayerInput]>
+    serverTick: number
+    clientTick: number
 }
 
 export type Message =
-  | LOG_MESSAGE
-  | JOIN_PARTY_MESSAGE
-  | PLAYER_INPUTS
-  | SERVER_TICK_MESSAGE
-  | CLIENT_TICK_MESSAGE
-  | HEARTBEAT_MESSAGE
+    | LOG_MESSAGE
+    | JOIN_PARTY_MESSAGE
+    | PLAYER_INPUTS
+    | SERVER_TICK_MESSAGE
+    | CLIENT_TICK_MESSAGE
+    | HEARTBEAT_MESSAGE
 
 export function log(message: string): LOG_MESSAGE {
-  return { type: "LOG", message }
+    return { type: 'LOG', message }
 }
 
-export function heartbeat(clientTick: number, serverTick: number): HEARTBEAT_MESSAGE {
-  return { type: "CLIENT_HEARTBEAT", serverTick, clientTick }
+export function heartbeat(
+    clientTick: number,
+    serverTick: number,
+): HEARTBEAT_MESSAGE {
+    return { type: 'CLIENT_HEARTBEAT', serverTick, clientTick }
 }
 
-export function joinParty(peerId: string, playerName: string): JOIN_PARTY_MESSAGE {
-  return { type: "JOIN_PARTY", playerName, peerId }
+export function joinParty(
+    peerId: string,
+    playerName: string,
+): JOIN_PARTY_MESSAGE {
+    return { type: 'JOIN_PARTY', playerName, peerId }
 }
 
 /* State Types*/
 export type stateT = {
-  parties: { [id: string]: Party }
-  games: { [id: string]: World }
-  clientTicks: {
-    [id: string]: { clientTick: number; ackedServerTick: number }
-  }
+    parties: { [id: string]: Party }
+    games: { [id: string]: World }
+    clientTicks: {
+        [id: string]: { clientTick: number; ackedServerTick: number }
+    }
 }
 
 export type Party = {
-  players: Array<Player>
-  status: "NOT_STARTED" | "PLAYING" | "FINISHED"
-  serverTick: number
+    players: Array<Player>
+    status: 'NOT_STARTED' | 'PLAYING' | 'FINISHED'
+    serverTick: number
 }
 
 export type Player = {
-  playerName: string
-  peerId: string
+    playerName: string
+    peerId: string
 }
 
 /* State Handlers */
 
 let state: stateT = {
-  parties: {},
-  games: {},
-  clientTicks: {},
+    parties: {},
+    games: {},
+    clientTicks: {},
 }
 
 let partyIndex: any = {}
 let nextParty = 0
 export function initTicks(peerId: string) {
-  state.clientTicks[peerId] = {
-    ackedServerTick: -1,
-    clientTick: -1,
-  }
+    state.clientTicks[peerId] = {
+        ackedServerTick: -1,
+        clientTick: -1,
+    }
 }
 
 function handleJoinParty(peerId: string, playerName: string) {
-  let partyId = Object.keys(state.parties).find(partyId => {
-    const party = state.parties[partyId]
-    return party.status === "NOT_STARTED" && party.players.length < maxPartySize
-  })
+    let partyId = Object.keys(state.parties).find(partyId => {
+        const party = state.parties[partyId]
+        return (
+            party.status === 'NOT_STARTED' &&
+            party.players.length < maxPartySize
+        )
+    })
 
-  if (partyId) {
-    const party = state.parties[partyId]
-    party.players.push({ peerId, playerName })
-    party.serverTick = serverTick
-    if (party.players.length === maxPartySize) {
-      party.status = "PLAYING"
-      state.games[partyId] = {
-        players: {
-          [party.players[0].peerId]: getDefaultPlayer(1, party.players[0].playerName),
-          [party.players[1].peerId]: getDefaultPlayer(2, party.players[1].playerName),
-          ["gibbersih"]: getDefaultPlayer(3, "fakePlayer3"),
-          ["gibberish2"]: getDefaultPlayer(4, "fakePlayer4"),
-        },
-        serverTick,
-      }
+    if (partyId) {
+        const party = state.parties[partyId]
+        party.players.push({ peerId, playerName })
+        party.serverTick = serverTick
+        if (party.players.length === maxPartySize) {
+            party.status = 'PLAYING'
+            state.games[partyId] = {
+                players: {
+                    [party.players[0].peerId]: getDefaultPlayer(
+                        1,
+                        party.players[0].playerName,
+                    ),
+                    [party.players[1].peerId]: getDefaultPlayer(
+                        2,
+                        party.players[1].playerName,
+                    ),
+                    ['gibbersih']: getDefaultPlayer(3, 'fakePlayer3'),
+                    ['gibberish2']: getDefaultPlayer(4, 'fakePlayer4'),
+                },
+                serverTick,
+            }
+        }
+    } else {
+        partyId = String(nextParty++)
+        state.parties[partyId] = {
+            status: 'NOT_STARTED',
+            players: [{ peerId, playerName }],
+            serverTick,
+        }
     }
-  } else {
-    partyId = String(nextParty++)
-    state.parties[partyId] = {
-      status: "NOT_STARTED",
-      players: [{ peerId, playerName }],
-      serverTick,
-    }
-  }
-  partyIndex[peerId] = partyId
-  console.log(`joining: ${partyId}`)
+    partyIndex[peerId] = partyId
+    console.log(`joining: ${partyId}`)
 }
 
 export function handleMessage(message: Message, peerId: string) {
-  if (message.type === "JOIN_PARTY") {
-    handleJoinParty(peerId, message.playerName)
-  } else if (message.type === "LOG") {
-    console.log(message.message)
-  } else if (message.type === "CLIENT_HEARTBEAT") {
-    const ticks = state.clientTicks[peerId]
-    ticks.clientTick = Math.max(ticks.clientTick, message.clientTick)
-    ticks.ackedServerTick = Math.max(ticks.ackedServerTick, message.serverTick)
-  } else if (message.type === "CLIENT_TICK") {
-    if (message.clientTick <= state.clientTicks[peerId].clientTick) {
-      console.log(`dupe or outdated message because of outdated clientTick: ${message.clientTick}`)
+    if (message.type === 'JOIN_PARTY') {
+        handleJoinParty(peerId, message.playerName)
+    } else if (message.type === 'LOG') {
+        console.log(message.message)
+    } else if (message.type === 'CLIENT_HEARTBEAT') {
+        const ticks = state.clientTicks[peerId]
+        ticks.clientTick = Math.max(ticks.clientTick, message.clientTick)
+        ticks.ackedServerTick = Math.max(
+            ticks.ackedServerTick,
+            message.serverTick,
+        )
+    } else if (message.type === 'CLIENT_TICK') {
+        if (message.clientTick <= state.clientTicks[peerId].clientTick) {
+            console.log(
+                `dupe or outdated message because of outdated clientTick: ${message.clientTick}`,
+            )
+        }
+
+        const partyId = getPartyId(peerId)
+        if (!partyId) {
+            console.log(`SHOULD NOT BE RECEIVING CLIENT_TICK BEFORE GAME START`)
+            return
+        }
+
+        const prevClientTick = state.clientTicks[peerId].clientTick
+        const clientTick = message.clientTick
+        state.clientTicks[peerId] = {
+            clientTick,
+            ackedServerTick: message.serverTick,
+        }
+
+        // This is awful anti-cheat logic. Right now clients can make 4 moves every move.
+        // TODO: use bursty api logic. hold a counter from 0 that increments up by one every 16ms w/ a low max. that num represents how many moves a client can make.
+        let inputs: Array<PlayerInput> = message.inputs
+            .filter(elem => elem[0] > prevClientTick)
+            .map(elem => elem[1])
+        inputs = _.takeRight(inputs, 5)
+
+        stepPlayer(state.games[partyId], peerId, inputs)
+        state.games[partyId].serverTick = serverTick
     }
-
-    const partyId = getPartyId(peerId)
-    if (!partyId) {
-      console.log(`SHOULD NOT BE RECEIVING CLIENT_TICK BEFORE GAME START`)
-      return
-    }
-
-    const prevClientTick = state.clientTicks[peerId].clientTick
-    const clientTick = message.clientTick
-    state.clientTicks[peerId] = { clientTick, ackedServerTick: message.serverTick }
-
-    // This is awful anti-cheat logic. Right now clients can make 4 moves every move.
-    // TODO: use bursty api logic. hold a counter from 0 that increments up by one every 16ms w/ a low max. that num represents how many moves a client can make.
-    let inputs: Array<PlayerInput> = message.inputs
-      .filter(elem => elem[0] > prevClientTick)
-      .map(elem => elem[1])
-    inputs = _.takeRight(inputs, 5)
-
-    stepPlayer(state.games[partyId], peerId, inputs)
-    state.games[partyId].serverTick = serverTick
-  }
 }
 
 /**
@@ -171,39 +198,42 @@ export function handleMessage(message: Message, peerId: string) {
  */
 
 function getPartyId(peerId: string) {
-  return partyIndex[peerId]
+    return partyIndex[peerId]
 }
 
-function getGameDataToSend(peerId: string, partyId: string): { world: World; party: Party } {
-  if (!peerId || !partyId) {
-    // throw new Error(`peerId: ${peerId} hostId: ${hostId}`)
-    return { world: null, party: null }
-  }
+function getGameDataToSend(
+    peerId: string,
+    partyId: string,
+): { world: World; party: Party } {
+    if (!peerId || !partyId) {
+        // throw new Error(`peerId: ${peerId} hostId: ${hostId}`)
+        return { world: null, party: null }
+    }
 
-  const { ackedServerTick } = state.clientTicks[peerId]
-  const ret: any = { world: null, party: null }
-  if (ackedServerTick <= _.get(state.parties, [partyId, "serverTick"])) {
-    ret.party = state.parties[partyId]
-  }
+    const { ackedServerTick } = state.clientTicks[peerId]
+    const ret: any = { world: null, party: null }
+    if (ackedServerTick <= _.get(state.parties, [partyId, 'serverTick'])) {
+        ret.party = state.parties[partyId]
+    }
 
-  if (ackedServerTick <= _.get(state.games, [partyId, "serverTick"])) {
-    ret.world = state.games[partyId]
-  }
-  return ret
+    if (ackedServerTick <= _.get(state.games, [partyId, 'serverTick'])) {
+        ret.world = state.games[partyId]
+    }
+    return ret
 }
 
 function getHeartbeatDataToSend(peerId: string) {
-  return { serverTick, clientTick: state.clientTicks[peerId].clientTick }
+    return { serverTick, clientTick: state.clientTicks[peerId].clientTick }
 }
 
 export function getTickData(peerId: string): SERVER_TICK_MESSAGE {
-  return {
-    type: "SERVER_TICK",
-      ...getHeartbeatDataToSend(peerId),
-      ...getGameDataToSend(peerId, getPartyId(peerId)), 
-  }
+    return {
+        type: 'SERVER_TICK',
+        ...getHeartbeatDataToSend(peerId),
+        ...getGameDataToSend(peerId, getPartyId(peerId)),
+    }
 }
 
 export function tick() {
-  serverTick++; 
+    serverTick++
 }
